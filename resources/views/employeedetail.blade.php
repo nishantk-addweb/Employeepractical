041a76a930b8a9ac2849829c2bc7e20c520674cd
nishantk-addweb <68736299+nishantk-addweb@users.noreply.github.com>
2020-09-03 19:59:08 +0530

    Update employeedetail.blade.php

--- resources/views/employeedetail.blade.php
+++ resources/views/employeedetail.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 6 Ajax CRUD tutorial using Datatable - ItSolutionStuff.com</title>
+    <title>Employee Details</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
